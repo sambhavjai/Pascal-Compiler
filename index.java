510597c0582274120ce2bc38d38c014f516d9d2e
sambhavjai <38015768+sambhavjai@users.noreply.github.com>
2020-05-27 00:22:11 +0530

    added classes

---
+++ index.java
@@ -0,0 +1,8 @@
+//import token;
+public class index{
+    public static void main(String args[])
+    {
+        token obj=new token("integer","3");
+        System.out.println(obj.str());
+    }
+}
--- token.java
+++ token.java
@@ -1,13 +1,4 @@
-public static class token{
-       public static String integer="INTEGER";
-	    public static String plus="PLUS";
-	    public static String eof="EOF";
-	    public static String minus="MINUS";
-	    public static String multiply="MULTIPLY";
-		public static String divide="DIVIDE";
-		public static String lparen="LPAREN";
-		public static String rparen="RPAREN";
-	    public static class token{
+public class token{
 	        String type;
 	        String value;
 	        public token(String type,String value)
@@ -20,4 +11,3 @@ public static class token{
 	            return "Token("+this.type+" , "+this.value+")";
 	        }
 	    }
-}
